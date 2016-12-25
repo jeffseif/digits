ac10fbb95b3ca7143385a20bb5aad64e0a0e4747
Jeffrey Edwin Seifried <jeffrey.seifried@gmail.com>
2016-12-25 14:50:10 -0500

    Removed docker stuff, since the repo seems to work reasonably well in a virtualenv

--- scripts/docker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-CONTAINERS="$(docker ps | grep digits | cut -f1 -d' ')" ;
-[ -z ${CONTAINERS} ] && docker kill "${CONTAINERS}" ;
-docker build --tag digits . ;
-docker run --tty digits ;
